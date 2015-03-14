f27705f79688f02b0f73616e2a53d747773e55df
Charles Hooper <charles@heroku.com>
2015-03-13 23:35:21 -0700

    add inline documentation. fixes #1

--- lib/rollout-dynamodb.rb
+++ lib/rollout-dynamodb.rb
@@ -1,4 +1,7 @@
 class Rollout
+
+  ##
+  # This module contains all of the DynamoDB related code
   module DynamoDB
     VERSION = "0.0.3"
   end
--- lib/rollout/dynamodb/storage.rb
+++ lib/rollout/dynamodb/storage.rb
@@ -1,8 +1,21 @@
 
 require 'fog'
 
+##
+# This module is a container for all things related to Rollout+DynamoDB functionality
 module Rollout::DynamoDB
+
+  ##
+  # This class implements the Storage API for Rollout
   class Storage
+
+    ##
+    # `options` is a Hash which may include:
+    # * `:connection_options` - a Hash which may in turn include:
+    #    * `:connect_timeout` - Connection timeout to DynamoDB in seconds (default: `3`)
+    #    * `:read_timeout`    - Read timeout to DynamoDB in seconds (default: `3`)
+    #    * `:write_timeout`   - Write timeout to DynamoDB in seconds (default: `3`)
+    #    * `:retry_limit`     - Number of times to retry HTTP requests to DynamoDB (default: `4`)
     def initialize(aws_access_key, aws_secret_key, table_name, region, options = {})
       @aws_access_key   = aws_access_key
       @aws_secret_key   = aws_secret_key
@@ -18,21 +31,33 @@ module Rollout::DynamoDB
       @options[:connection_options][:retry_limit]     ||= 4
     end
 
+    ##
+    # Gets the value (*String*) of a key (*String*) from DynamoDB. Returns the
+    # value or raises an exception on error
     def get(key)
       response = db.get_item(@table_name, {'HashKeyElement' => {'S' => key}})
       get_item_from_body(response.body)
     end
 
+    ##
+    # Sets the value (*String*) of a key (*String*) in DynamoDB. Returns `true`
+    # on success, raises an exception on error
     def set(key, value)
       response = db.put_item(@table_name, {'id' => {'S' => key}, 'value' => {'S' => value}})
       true
     end
 
+    ##
+    # Deletes a key (*String*) from DynamoDB. Returns `true` on success or
+    # raises an exception on errror
     def del(key)
       response = db.delete_item(@table_name, {'HashKeyElement' => {'S' => key}})
       true
     end
 
+    ##
+    # Returns an `Array` of keys (`String`) from DynamoDB. Not used by the
+    # Storage API, but provided as a convenience and used in tests.
     def list
       response = db.scan(@table_name)
       response.body['Items'].map { |x| get_key_from_result(x) }
@@ -40,6 +65,8 @@ module Rollout::DynamoDB
 
     private
 
+    ##
+    # Returns the DynamoDB connection object
     def db
       @db ||= Fog::AWS::DynamoDB.new(
         @options.merge(
@@ -48,14 +75,20 @@ module Rollout::DynamoDB
           :region => @region))
     end
 
+    ##
+    # Extracts and returns a key (*String*) from a DynamoDB result set. May be `nil`.
     def get_key_from_result(res)
       res.fetch('id', {}).fetch('S', nil)
     end
 
+    ##
+    # Extracts and returns a value (*String*) from a DynamoDB result set. May be `nil`.
     def get_item_from_result(res)
       res.fetch('value', {}).fetch('S', nil)
     end
 
+    ##
+    # Returns an item (*Hash*) from a DynamoDB result body
     def get_item_from_body(body)
       get_item_from_result(body.fetch('Item', {}))
     end
